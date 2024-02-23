31ef02e03bda810c800ad52622d67c951c35c314
uchujin1221 <bosco.mochizuki@gmail.com>
2024-02-23 21:35:26 +0900

    update comment for HashTable.js

--- 25-Hash-Tables/HashTable.js
+++ 25-Hash-Tables/HashTable.js
@@ -48,7 +48,7 @@ class HashTable {
       : undefined;
   }
 
-  // Loop through the hash table and return an array of keys in the table
+  // Loop through the hash table and return an array of keys in the table (distinct)
   keys() {
     const retVal = new Set();
     this.keyMap.map((subarray) =>
@@ -57,7 +57,7 @@ class HashTable {
     return retVal;
   }
 
-  // Loop through the hash table and return an array of values in the table (distict values)
+  // Loop through the hash table and return an array of values in the table (distinct)
   values() {
     const retVal = new Set();
     this.keyMap.map((subarray) =>
